4b6da12f948f81c219d6d82030c7248b74261650
173951428 <173951428@qq.com>
2019-10-05 02:15:12 +0800

    新增加zuul 网关服务，并且对url进行校验

---
+++ springcloud_gateway/src/main/java/com/springcloud/springcloud_gateway/SpringcloudGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.springcloud.springcloud_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy // 启动类加入网关注解
+public class SpringcloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudGatewayApplication.class, args);
+    }
+
+}
